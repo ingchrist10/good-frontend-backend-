bdcd8d6f4704fd516242e8caf7c82caa69d52d39
ingchrist1 <cdjinguet1@gmail.com>
2025-05-28 18:40:34 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- analytix_hive_backend/settings.py
+++ analytix_hive_backend/settings.py
@@ -46,3 +46,5 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
