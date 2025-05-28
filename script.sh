553560c40e285407dd1694a062a164efafd2a7e9
ingchrist1 <cdjinguet1@gmail.com>
2025-05-28 18:28:02 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- script.sh
+++ script.sh
@@ -10,8 +10,4 @@ echo "Watching $WATCH_DIR for changes..."
 while inotifywait -r -e modify,create,delete,move .; do
     git add .
     git commit -m "in the mist of doing hard things by ingchrist: file/folder change detected"
-    git push
-    echo "Changes pushed to GitHub."
-    sleep 10 # <--- Added: Sleep for 10 seconds after each push
-    echo "Resuming watch after 10-second pause."
-done
+    
