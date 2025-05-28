2d551421a48bdf4cbe91cb8c838f0ef0d15fb548
ingchrist1 <cdjinguet1@gmail.com>
2025-05-28 18:28:14 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- script.sh
+++ script.sh
@@ -6,8 +6,3 @@ WATCH_DIR="."
 cd "$WATCH_DIR" || exit 1
 
 echo "Watching $WATCH_DIR for changes..."
-
-while inotifywait -r -e modify,create,delete,move .; do
-    git add .
-    git commit -m "in the mist of doing hard things by ingchrist: file/folder change detected"
-    
