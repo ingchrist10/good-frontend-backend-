10621f4b63713ca2b0c90ee663fa567e9384ac26
ingchrist1 <cdjinguet1@gmail.com>
2025-05-28 18:34:06 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- analytix_hive_backend/settings.py
+++ analytix_hive_backend/settings.py
@@ -1,13 +1,32 @@
 # settings.py
 
-INSTALLED_APPS += [
+INSTALLED_APPS = [
+    # Default Django apps...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Third-party apps
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    # Local apps
     'authentication',
 ]
 
-MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    # Default Django middleware...
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
