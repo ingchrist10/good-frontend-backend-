c75942455f5669b02b0f2666d273881e3b9e3555
ingchrist1 <cdjinguet1@gmail.com>
2025-05-28 18:43:34 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- analytix_hive_backend/authentication/models.py
+++ analytix_hive_backend/authentication/models.py
@@ -1,7 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_set",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_set",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
