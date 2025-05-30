6d39932b4b290aa7805cc102b0ec014c6af05007
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 19:37:39 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- authentication/views.py
+++ authentication/views.py
@@ -4,11 +4,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
-from .serializers import UserSerializer
+from .serializers import UserSerializer, GoogleAuthSerializer
+from django.conf import settings
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
-from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class LoginRateThrottle(AnonRateThrottle):
@@ -29,46 +29,45 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = settings.GOOGLE_CALLBACK_URL
+    
+    def get_response(self):
+        response = super().get_response()
+        if self.user:
+            # Update or create user profile data from Google
+            social_account = self.user.socialaccount_set.get(provider='google')
+            extra_data = social_account.extra_data
+            
+            self.user.google_id = extra_data.get('sub')
+            self.user.profile_picture = extra_data.get('picture')
+            self.user.first_name = extra_data.get('given_name', '')
+            self.user.last_name = extra_data.get('family_name', '')
+            self.user.save()
+            
+            # Add user data to response
+            response.data['user'] = {
+                'id': self.user.id,
+                'email': self.user.email,
+                'first_name': self.user.first_name,
+                'last_name': self.user.last_name,
+                'profile_picture': self.user.profile_picture
+            }
+        return response
+
 class ProtectedView(APIView):
     permission_classes = [IsAuthenticated]
     throttle_classes = [UserRateThrottle]
 
     def get(self, request):
-        return Response({"message": "This is a protected endpoint."})
-
-class GoogleLoginView(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
-    callback_url = settings.GOOGLE_CALLBACK_URL
-    client_class = OAuth2Client
-
-class GoogleAuthCallbackView(APIView):
-    def get(self, request, *args, **kwargs):
-        code = request.GET.get('code')
-        if not code:
-            return Response({'error': 'No authorization code provided'},
-                          status=status.HTTP_400_BAD_REQUEST)
-        
-        try:
-            # Process the authorization code and get user info
-            adapter = GoogleOAuth2Adapter(request)
-            access_token = adapter.get_access_token(code)
-            user_data = adapter.get_user_info(access_token)
-            
-            # Create or get user
-            user = adapter.get_user(user_data)
-            
-            # Generate JWT tokens
-            refresh = RefreshToken.for_user(user)
-            
-            return Response({
-                'access': str(refresh.access_token),
-                'refresh': str(refresh),
-                'user': {
-                    'id': user.id,
-                    'email': user.email,
-                    'username': user.username
-                }
-            })
-        except Exception as e:
-            return Response({'error': str(e)},
-                          status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            "message": "This is a protected endpoint.",
+            "user": {
+                "id": request.user.id,
+                "email": request.user.email,
+                "username": request.user.username,
+                "profile_picture": request.user.profile_picture
+            }
+        })
