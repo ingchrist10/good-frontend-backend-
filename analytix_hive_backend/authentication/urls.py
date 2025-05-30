f3de278ca882f0a6744cf4e06cf8b9d9e508ccee
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 19:20:31 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- analytix_hive_backend/authentication/urls.py
+++ analytix_hive_backend/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, ProtectedView
+from .views import RegisterView, ProtectedView, GoogleLoginView, GoogleAuthCallbackView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
+    # Google OAuth URLs
+    path('google/', GoogleLoginView.as_view(), name='google_login'),
+    path('google/callback/', GoogleAuthCallbackView.as_view(), name='google_callback'),
 ]
