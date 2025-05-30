0fd2ea0a56938b1495458df97b1689da79cca16f
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 19:57:55 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,23 +1,22 @@
-from django.urls import path, re_path
-from .views import RegisterView, ProtectedView, GoogleLogin, GoogleCallback
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from dj_rest_auth.registration.views import (
-    SocialAccountListView, SocialAccountDisconnectView
+from django.urls import path
+from .views import (
+    RegisterView, 
+    ProtectedView, 
+    GoogleLoginView, 
+    GoogleCallback
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Registration and protected endpoints
+    path('register/', RegisterView.as_view(), name='register'),
     path('protected/', ProtectedView.as_view(), name='protected'),
     
-    # Google OAuth URLs
-    path('google/', GoogleLogin.as_view(), name='google_login'),
+    # Google authentication endpoints
+    path('google/login/', GoogleLoginView.as_view(), name='google_login'),
     path('google/callback/', GoogleCallback.as_view(), name='google_callback'),
-    path('social/accounts/', SocialAccountListView.as_view(), name='social_account_list'),
-    re_path(
-        r'^social/accounts/(?P<pk>\d+)/disconnect/$',
-        SocialAccountDisconnectView.as_view(),
-        name='social_account_disconnect'
-    ),
 ]
