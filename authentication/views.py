4b398595592b253c60d9014d6897f2a01bef152b
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 23:16:52 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
