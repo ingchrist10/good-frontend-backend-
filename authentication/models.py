9bc0ae80b543d85e2fb0d7505cd6ccc870b5f86c
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 19:37:01 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,10 @@ from django.db import models
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    # Add custom fields
+    google_id = models.CharField(max_length=150, blank=True, null=True)
+    profile_picture = models.URLField(max_length=500, blank=True, null=True)
+    
     groups = models.ManyToManyField(
         Group,
         related_name="customuser_set",
@@ -18,3 +22,6 @@ class CustomUser(AbstractUser):
         help_text="Specific permissions for this user.",
         verbose_name="user permissions",
     )
+
+    def __str__(self):
+        return self.email or self.username
