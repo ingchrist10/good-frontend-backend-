8a654001fce6a636c60e65de46e7b53171b13d4a
ingchrist1 <cdjinguet1@gmail.com>
2025-05-30 19:37:55 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,13 +1,22 @@
-from django.urls import path
-from .views import RegisterView, ProtectedView, GoogleLoginView, GoogleAuthCallbackView
+from django.urls import path, re_path
+from .views import RegisterView, ProtectedView, GoogleLogin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from dj_rest_auth.registration.views import (
+    SocialAccountListView, SocialAccountDisconnectView
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
+    
     # Google OAuth URLs
-    path('google/', GoogleLoginView.as_view(), name='google_login'),
-    path('google/callback/', GoogleAuthCallbackView.as_view(), name='google_callback'),
+    path('google/', GoogleLogin.as_view(), name='google_login'),
+    path('social/accounts/', SocialAccountListView.as_view(), name='social_account_list'),
+    re_path(
+        r'^social/accounts/(?P<pk>\d+)/disconnect/$',
+        SocialAccountDisconnectView.as_view(),
+        name='social_account_disconnect'
+    ),
 ]
