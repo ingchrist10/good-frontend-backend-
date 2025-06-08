d8b3ad76c1d0c32f57eb5020239603f5b664e32c
ingchris10 <ingchrist10@gmail.com>
2025-06-08 17:29:08 +0000

    ok

--- frontend-codes/next.config.ts
+++ frontend-codes/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // This allows requests from your Cloud Shell preview URL
+    allowedDevOrigins: ["https://3001-cs-88a5d951-d78f-4ca5-b4dc-e151e80a6bca.cs-europe-west1-xedi.cloudshell.dev"],
+  },
 };
 
 export default nextConfig;
